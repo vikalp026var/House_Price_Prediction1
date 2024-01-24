cf2d66f38aad92a7fa3c623bff51e87e53ca8ac6
VIKALP <113850666+vikalp026var@users.noreply.github.com>
2024-01-24 23:01:53 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+     name="House Price Prediction",
+     version="0.0.1",
+     author="Vikalp",
+     author_email="vikalp026varshney@gmail.com",
+     package_dir=find_packages(),
+     include_dirs=[]
+)
